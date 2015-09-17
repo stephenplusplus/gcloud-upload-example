1d6cbecb31aa2dcd98d1c8177e6c474b80b5d1f5
Stephen Sawchuk <sawchuk@gmail.com>
2015-09-17 10:15:30 -0400

    use file downloader

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var async = require('async');
 var express = require('express');
+var levelup = require('levelup');
 var multer = require('multer');
 
 var gcloud = require('gcloud')({
@@ -16,7 +18,10 @@ app.set('view engine', 'html');
 app.listen(8080);
 
 app.get('/', function(req, res) {
-  res.render('index', { simple: false, complex: false });
+  res.render('index', {
+    simple: false,
+    complex: false
+  });
 });
 
 // ---------------
@@ -77,5 +82,78 @@ app.get('/complex', function(req, res) {
 });
 
 app.post('/upload', upload.single('file'), function(req, res) {
-  res.redirect('/');
+  // Display a list of files from the bucket after uploading is complete.
+  res.redirect('/files');
+});
+
+// ------------------------
+// STREAMING FILE DOWNLOAD.
+// ------------------------
+// This opens a readable stream from the file, then pipes it to the response.
+app.get('/file/:filename', function(req, res) {
+  var file = bucket.file(req.params.filename);
+  file.createReadStream().pipe(res);
+});
+
+// --------------------------
+// GCS-BACKED FILE DOWNLOADS.
+// --------------------------
+// This route will get all of the files in a bucket, generate a signed URL for
+// each, then render the front end which will display the download links.
+//
+// The signed URLs are created once, then stored in a local database using
+// levelup -- http://gitnpm.com/levelup.
+var db = levelup('./filename-to-url');
+
+app.get('/files', function(req, res) {
+  bucket.getFiles(function(err, files) {
+    if (err) {
+      res.emit('error', err);
+      res.end();
+      return;
+    }
+
+    async.map(files, assignSignedUrl, function(err, files) {
+      if (err) {
+        res.emit('error', err);
+        res.end();
+        return;
+      }
+
+      res.render('files', {
+        files: files
+      });
+    });
+  });
+
+  function assignSignedUrl(file, callback) {
+    // Check if we have already assigned a signed URL for this file.
+    db.get(file.name, function(err, value) {
+      if (value) {
+        file.signedUrl = value;
+        callback(null, file);
+        return;
+      }
+
+      file.getSignedUrl({
+        action: 'read',
+        expires: '2025'
+      }, function(err, url) {
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        db.put(file.name, url, function(err) {
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          file.signedUrl = value;
+          callback(null, file);
+        });
+      });
+    });
+  }
 });
